de8e6c9796fd2669bef2c81277ad2d4055b8e600
Lidia <necula_lidia@yahoo.com>
2020-12-24 23:01:31 +0400

    Add repo implementation for Group

---
+++ src/Infrastructure/Repositories/GroupRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Domain.Common;
+using Domain.Groups;
+
+namespace Infrastructure.Repositories
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly ISmartChargingDbContext _context;
+
+        public IUnitOfWork UnitOfWork => _context;
+
+        public GroupRepository(ISmartChargingDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Group> AddAsync(Group group)
+        {
+            var entity = await _context.Groups.AddAsync(group);
+
+            await _context.SaveChangesAsync();
+
+            return entity.Entity;
+        }
+    }
+}
