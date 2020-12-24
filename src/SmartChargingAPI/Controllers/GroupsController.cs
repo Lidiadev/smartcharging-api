013bcaf5900f8660b64c0743325b3e1908548016
Lidia <necula_lidia@yahoo.com>
2020-12-24 22:39:56 +0400

    Add controller for Groups

---
+++ src/SmartChargingAPI/Controllers/GroupsController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Groups.CreateGroup;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace SmartChargingAPI.Controllers
+{
+    public class GroupsController : ApiController
+    {
+        private readonly ILogger<GroupsController> _logger;
+
+        public GroupsController(ILogger<GroupsController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<ActionResult<long>> Create(CreateGroupCommand command)
+        {
+            var id = await Mediator.Send(command);
+
+            _logger.LogInformation($"Group {id} was created.");
+
+            return id;
+        }
+    }
+}
