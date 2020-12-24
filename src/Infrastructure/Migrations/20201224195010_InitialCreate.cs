60af14b5883a9ae1f2490639ae4d247ad62a19aa
Lidia <necula_lidia@yahoo.com>
2020-12-25 00:00:42 +0400

    Add initial create migration

---
+++ src/Infrastructure/Migrations/20201224195010_InitialCreate.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "chargestationseq",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "connectorseq",
+                incrementBy: 10);
+
+            migrationBuilder.CreateSequence(
+                name: "groupseq",
+                incrementBy: 10);
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Capacity = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ChargeStation",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    GroupId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChargeStation", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ChargeStation_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connector",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    MaxCurrent = table.Column<int>(nullable: true),
+                    ChargeStationId = table.Column<long>(nullable: true),
+                    ChargeStationId1 = table.Column<long>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connector", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Connector_ChargeStation_ChargeStationId",
+                        column: x => x.ChargeStationId,
+                        principalTable: "ChargeStation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Connector_ChargeStation_ChargeStationId1",
+                        column: x => x.ChargeStationId1,
+                        principalTable: "ChargeStation",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChargeStation_GroupId",
+                table: "ChargeStation",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connector_ChargeStationId",
+                table: "Connector",
+                column: "ChargeStationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connector_ChargeStationId1",
+                table: "Connector",
+                column: "ChargeStationId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connector");
+
+            migrationBuilder.DropTable(
+                name: "ChargeStation");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropSequence(
+                name: "chargestationseq");
+
+            migrationBuilder.DropSequence(
+                name: "connectorseq");
+
+            migrationBuilder.DropSequence(
+                name: "groupseq");
+        }
+    }
+}
