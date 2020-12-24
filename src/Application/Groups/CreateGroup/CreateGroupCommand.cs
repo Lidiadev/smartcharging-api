ea57843713f1cf987ce59047703522e216d67a92
Lidia <necula_lidia@yahoo.com>
2020-12-24 22:26:29 +0400

    Add handler for creating a group

---
+++ src/Application/Groups/CreateGroup/CreateGroupCommand.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Groups;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Groups.CreateGroup
+{
+    public class CreateGroupCommand : IRequest<long>
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+
+    public class CreateGroupCommandHandler : IRequestHandler<CreateGroupCommand, long>
+    {
+        private readonly IGroupRepository _groupRepository;
+
+        public CreateGroupCommandHandler(IGroupRepository groupRepository)
+        {
+            _groupRepository = groupRepository ?? throw new ArgumentNullException(nameof(groupRepository));
+        }
+
+        public async Task<long> Handle(CreateGroupCommand request, CancellationToken cancellationToken)
+        {
+            var capacity = Ampere.Create(request.Capacity);
+
+            var entity = new Group(request.Name, capacity);
+
+            await _groupRepository.AddAsync(entity);
+
+            return entity.Id;
+        }
+    }
+}
