73d1dd4b58ec1dfe61562ba3b92548e9a39459f2
Lidia <necula_lidia@yahoo.com>
2020-12-24 21:47:38 +0400

    Add repo for Group

---
+++ src/Domain/Groups/IGroupRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Common;
+using System.Threading.Tasks;
+
+namespace Domain.Groups
+{
+    public interface IGroupRepository : IRepository<Group>
+    {
+        Task<Group> AddAsync(Group group);
+    }
+}
